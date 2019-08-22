f90fd0acb41e85d8808e0a0b3e7723c7b189c90f
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-22 14:46:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+def map(source_array)
+source_array.map { |number| number * -1}
+end
+def map
+  source_array.map do |number| number
+end
+end
+def multiply(source_array)
+  source_array.map do |number| number *2
+end
+end
+
+def squire(source_array)
+  source_array.map do |number| number * number
+end
+end
+
+def reduce(source_array, starting_point = 0)
+  source_array.reduce(starting_point) { |sum, num| sum + num }
+end
+  def reduce(source_array)
+    source_array.reduce { |value, elem| value && elem}
+  end
+  def reduce(source_array)
+    source_array.any?(true)
+  end
+
+  
